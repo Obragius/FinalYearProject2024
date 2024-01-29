2e68ac25938a1796716e6cdb456a8e05b2483fdb
Obragius <lolkinadius@gmail.com>
2024-01-29 23:51:36 +0000

    Added the class diagram svg

--- ATC/src/main/java/dev/jamtech/ATC/AtcApplication.java
+++ ATC/src/main/java/dev/jamtech/ATC/AtcApplication.java
@@ -2,12 +2,21 @@ package dev.jamtech.ATC;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class AtcApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AtcApplication.class, args);
 	}
+        
+        @GetMapping("/root")
+        public String apiIndex()
+        {
+            return "Hello, this is my string";
+        }
 
 }
