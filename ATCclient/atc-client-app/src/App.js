efb18487be7d7f2f3936075279d307ab003723a4
DESKTOP-RNHNU2V\lolki <Obragius>
2024-02-14 21:33:25 +0000

    Loaded a basic map from streetmap in the react app.js

--- ATCclient/atc-client-app/src/App.js
+++ ATCclient/atc-client-app/src/App.js
@@ -1,25 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import api from './api/axiosConfig';
+import { useState, useEffect } from 'react';
+
+import { MapContainer, TileLayer } from 'react-leaflet';
+import "leaflet/dist/leaflet.css";
 
 function App() {
+  const [map, setMap] = useState();
+
+  const getMap = async () => {
+    try
+    {
+      const response = await api.get("/api/getMap");
+
+      console.log(response.data);
+
+      setMap(response.data);
+    }
+    catch (err)
+    {
+      console.log(err);
+    }
+  }
+
+  useEffect(() =>
+  {
+    getMap();
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <MapContainer center={[35.5140,5.1312]} zoom={13}>
+      <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' 
+      url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+
+      </MapContainer>
+  )
 }
 
 export default App;
