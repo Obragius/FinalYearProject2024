1b1c2f210024c7221306e62e591303ed9d948f09
Obragius <lolkinadius@gmail.com>
2024-06-06 18:09:58 +0100

    v0.7 Now commands can be cycled through with arrow keys in the chat window input

--- ATCclient/atc-client-app/src/components/ChatWindow.js
+++ ATCclient/atc-client-app/src/components/ChatWindow.js
@@ -2,6 +2,9 @@ import { useState, useEffect } from 'react';
 
 var key= 0;
 
+var messageList = [];
+var currentMessageIndex = -1;
+
 function ChatWindow({api,mapID,text,chatValue,formValue,setFormValue})
 {
     useEffect (() => {const interval = setInterval(() => {HandleInput();
@@ -14,48 +17,81 @@ function ChatWindow({api,mapID,text,chatValue,formValue,setFormValue})
     }
 
     const HandleInput = async(e) =>
-  {
-    if(document.getElementById("input").innerHTML !== "")
     {
-      var newText = document.getElementById("input").innerHTML;
-      document.getElementById("input").innerHTML = "";
-      const response = await api.post("api/addCommand",{"mapID":mapID,"text":newText});
-      console.log(response.data);
-      var Message = [newText.toUpperCase(),"Command"];
-      chatValue.push(Message);
-      if (response.data !== "Aircraft not found")
-      { 
-        Message = [response.data.toUpperCase(),"Response"];
-        chatValue.push(Message);
-      }
-      setFormValue('')
-    }
-  }
-    
-    const sendCommand = async(e) => {
-        e.preventDefault();
-        const response = await api.post("api/addCommand",{"mapID":mapID,"text":formValue});
+      if(document.getElementById("input").innerHTML !== "")
+      {
+        // v0.7 There will be a list of commands that have been inputed and they can be cycled through
+        messageList.unshift(formValue);
+        currentMessageIndex = -1;
+        // -------------------------------------------------------------------------------------------
+        var newText = document.getElementById("input").innerHTML;
+        document.getElementById("input").innerHTML = "";
+        const response = await api.post("api/addCommand",{"mapID":mapID,"text":newText});
         console.log(response.data);
-        var target = formValue.split(" ")[0]
-        if (response.status < 300 )
+        var Message = [newText.toUpperCase(),"Command"];
+        chatValue.push(Message);
+        if (response.data !== "Aircraft not found")
         { 
-          var Message = [formValue.toUpperCase(),"Command",key,target];
-          chatValue.push(Message);
-          key += 1;
-          Message = [response.data.toUpperCase(),"Response",key,target];
+          Message = [response.data.toUpperCase(),"Response"];
           chatValue.push(Message);
         }
         setFormValue('')
-        key += 1;
       }
+    }
+    
+    const sendCommand = async(e) => 
+    {
+      e.preventDefault();
+      // v0.7 There will be a list of commands that have been inputed and they can be cycled through
+      messageList.unshift(formValue);
+      currentMessageIndex = -1;
+      // -------------------------------------------------------------------------------------------
+      const response = await api.post("api/addCommand",{"mapID":mapID,"text":formValue});
+      console.log(response.data);
+      var target = formValue.split(" ")[0]
+      if (response.status < 300 )
+      { 
+        var Message = [formValue.toUpperCase(),"Command",key,target];
+        chatValue.push(Message);
+        key += 1;
+        Message = [response.data.toUpperCase(),"Response",key,target];
+        chatValue.push(Message);
+      }
+      setFormValue('')
+      key += 1;
+    }
+
+    // v0.7 Cycle through all inputs
+    const loadPreviosMessage = (event) =>
+    {
+      if (event.key == "ArrowUp")
+        {
+          if (currentMessageIndex < messageList.length-1)
+          {
+            currentMessageIndex += 1;
+          }
+          setFormValue(messageList[currentMessageIndex]);
+        }
+      if (event.key == "ArrowDown")
+        {
+          if (currentMessageIndex > 0)
+          {
+            currentMessageIndex -= 1;
+          }
+          setFormValue(messageList[currentMessageIndex]);
+        }
+    }
+
+    // From v0.7 input field to detect key presses to enable up arrow to load previous messages
 
     return (<><div className='ChatWindow'>
             {text.map((message) => <div id={message[3]} onClick={(e) => SelectPlane(e)} key={message[2]} className={message[1]}><b>{"-"+message[0]}</b></div>)}
             </div>
             <form name="myform" onSubmit={sendCommand}>
-            <input className='Input' value={formValue} onChange={(e) => setFormValue(e.target.value)}></input>
+            <input id="CommandInput" onKeyDown={loadPreviosMessage} className='Input' value={formValue} onChange={(e) => setFormValue(e.target.value)}></input>
           </form>
-          <p hidden id="input"></p></>)
+          <p hidden id="input"></p>
+          </>)
 } 
 
 export default ChatWindow;
